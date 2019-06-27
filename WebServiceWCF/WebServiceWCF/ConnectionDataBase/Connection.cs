8fd8d3e3118919668fcb9ad04214a7cbc595b882
Claudia Carolina Jara Caceres <claudia.jara@neoris.cxnetworks.net>
2019-06-27 17:20:50 -0300

    Commit

---
+++ WebServiceWCF/WebServiceWCF/ConnectionDataBase/Connection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebServiceWCF.ConnectionDataBase
+{
+    public static class Connection
+    {
+
+        #region Properties
+        private static string _connectionString = ConfigurationManager.ConnectionStrings["DBAgenda"].ConnectionString;
+        private static SqlConnection _sqlConnection { get; set; }
+        #endregion
+
+        #region Public Methods
+        public static SqlConnection GetConnection()
+        {
+            if (_sqlConnection == null)
+            {
+                _sqlConnection = new SqlConnection(_connectionString);
+                return _sqlConnection;
+            }
+            else
+            {
+                return _sqlConnection;
+            }
+        }
+        #endregion
+    }
+}
---
+++ WebServiceWCF/WebServiceWCF/Models/Zonas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WebServiceWCF.Models
+{
+    [DataContract]
+    public class Zonas
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Zona { get; set; }
+    }
+}
